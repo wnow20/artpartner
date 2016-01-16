b983cb9d2cfeba9262fdc2622cc651d7c95ab267
wnow20 <wnow20@gmail.com>
2016-01-16 18:43:19 +0800

    add cache control for photos

--- routes/album.js
+++ routes/album.js
@@ -5,6 +5,8 @@ var Page = require(process.cwd() + '/lib/page');
 var DwzMsg = require(process.cwd() + '/lib/DwzMsg');
 var fs = require('fs');
 var Config = require(cwd + '/lib2/config.json');
+var mime = require('mime');
+var zlib = require('zlib');
 
 exports.index = function (req, res, next) {
     var uuid_ext = req.params.uuid_ext;
@@ -22,21 +24,38 @@ exports.index = function (req, res, next) {
         }
 
         var filepath = Config.upload_path + 'cover/' + uuid_ext;
-        fs.exists(filepath, function (exists) {
-            if (exists) {
-                var stats = fs.statSync(filepath);
-
-                res.setHeader('Last-Modified', stats.mtime);
-                res.setHeader('Cache-Control', 'public, max-age=0');
-                res.setHeader('X-Powered-By', 'Express');
-
-                var stream = fs.createReadStream(filepath);
-                stream.pipe(res);
-            } else {
-                console.error(new Error('File not found. path: ' + filepath));
-
-                res.send(404);
+        fs.stat(filepath, function (err, stats) {
+            if (err) {
+                return res.send(404);
             }
+
+            var lastModified = (new Date(stats.mtime)).toUTCString();
+            var acceptEncoding = req.headers['accept-encoding'] || "";
+
+            res.statusCode = 200;
+            if (req.headers["If-Modified-Since".toLowerCase()] == lastModified) {
+                res.statusCode = 304;
+                res.end();
+                return;
+            }
+
+            res.setHeader('Content-Length', stats.size);
+            res.setHeader('Content-Type', mime.lookup(filepath));
+            res.setHeader('last-modified', lastModified);
+            res.setHeader("Expires", (new Date((new Date()).getTime() + 60 * 60 * 24 * 30 * 1000)).toUTCString());
+            res.setHeader("Cache-Control", "max-age=" + (60 * 60 * 24 * 30 * 1000));
+
+            var stream = fs.createReadStream(filepath);
+
+            if (acceptEncoding.match(/\bgzip\b/)) {
+                res.setHeader('Content-Encoding', 'gzip');
+                stream = stream.pipe(zlib.createGzip());
+            } else if (acceptEncoding.match(/\bdeflate\b/)) {
+                res.setHeader('Content-Encoding', 'deflate');
+                stream = stream.pipe(zlib.createDeflate());
+            }
+
+            stream.pipe(res);
         });
     });
 };
@@ -56,15 +75,39 @@ exports.temp = function (req, res, next) {
         }
 
         var filepath = Config.upload_path + 'temp/' + uuid_ext;
-        fs.exists(filepath, function (exists) {
-            if (exists) {
-                var stream = fs.createReadStream(filepath);
-                stream.pipe(res);
-            } else {
-                console.error(new Error('File not found. path: ' + filepath));
 
-                res.send(404);
+        fs.stat(filepath, function (err, stats) {
+            if (err) {
+                return res.send(404);
             }
+
+            var lastModified = (new Date(stats.mtime)).toUTCString();
+            var acceptEncoding = req.headers['accept-encoding'] || "";
+
+            res.statusCode = 200;
+            if (req.headers["If-Modified-Since".toLowerCase()] == lastModified) {
+                res.statusCode = 304;
+                res.end();
+                return;
+            }
+
+            res.setHeader('Content-Length', stats.size);
+            res.setHeader('Content-Type', mime.lookup(filepath));
+            res.setHeader('last-modified', lastModified);
+            res.setHeader("Expires", (new Date((new Date()).getTime() + 60 * 60 * 24 * 30 * 1000)).toUTCString());
+            res.setHeader("Cache-Control", "max-age=" + (60 * 60 * 24 * 30 * 1000));
+
+            var stream = fs.createReadStream(filepath);
+
+            if (acceptEncoding.match(/\bgzip\b/)) {
+                res.setHeader('Content-Encoding', 'gzip');
+                stream = stream.pipe(zlib.createGzip());
+            } else if (acceptEncoding.match(/\bdeflate\b/)) {
+                res.setHeader('Content-Encoding', 'deflate');
+                stream = stream.pipe(zlib.createDeflate());
+            }
+
+            stream.pipe(res);
         });
     });
 };
